af53d13c5e1b07224838734dd1998c3b1578cf0f
pucher1983 <puchkov@jet.su>
2019-06-18 18:07:30 +0300

    Cosmetic update

--- Lab1.2/data_analyse.py
+++ Lab1.2/data_analyse.py
@@ -1,13 +1,14 @@
 from matplotlib import pyplot
 from openpyxl import load_workbook
 
-def getvalue(x):
-    return x.value
+
+def getvalue(x): return x.value
+
 
 wb = load_workbook('C:\\Users\\pucher\\Documents\\Seafile\\p4ne_training\\data_analysis_lab.xlsx')
 
 sheet = wb['Data']
-#sheet['A'][1:]
+# sheet['A'][1:]
 
 result_x = map(getvalue, sheet['A'][1:])
 list_x = list(result_x)
@@ -20,5 +21,5 @@ list_y2 = list(result_y2)
 
 pyplot.plot(list_x, list_y1, label="Метка1")
 pyplot.plot(list_x, list_y2, label="Метка2")
-
+pyplot.legend(loc='upper left')
 pyplot.show()
