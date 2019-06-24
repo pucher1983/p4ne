5ed9c1e41553da215c5c7a1b99947ad3c1d3ddd5
pucher1983 <puchkov@jet.su>
2019-06-25 00:54:14 +0300

    lab1.5

--- Lab1.5/textfile.py
+++ Lab1.5/textfile.py
@@ -1,14 +1,21 @@
 import glob
-import os
+import ipaddress
+
 
 lab_path = "C:\\Users\\pucher\\Documents\\Seafile\\p4ne_training\\config_files"
-
 file_list2 = glob.iglob(lab_path + "\\*.txt")
-
+dict_addr = {}
 for f in file_list2:
     with open(f) as file:
-        print(file.readlines())
-
-
-
-#print(glob.glob0("C:\\Users\\pucher\\Documents\\Seafile\\p4ne_training\\config_files","*.txt"))
+        content = file.readlines()
+        for line in content:
+            if line.find('ip address ') > 0:
+                temp_arrd = line.strip()[11:]
+                try:
+                    addr = ipaddress.IPv4Address(temp_arrd.split(' ')[0])
+                except ipaddress.AddressValueError:
+                    pass
+                else:
+                    if not temp_arrd in dict_addr:
+                        dict_addr[temp_arrd]=1
+print(sorted(dict_addr.keys()))
